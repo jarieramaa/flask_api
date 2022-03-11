c227368a0ba6cd354667e8ad311b118d3b21d9a7
Jari Erämaa <jari@Jaris-MacBook-Pro.local>
2022-03-11 10:26:14 +0100

    Small changes, cleaning the code. There was one function that was no longer needed, Also black-formatting

--- flask_challenge.py
+++ flask_challenge.py
@@ -13,26 +13,32 @@ app = Flask(__name__)
 @app.route("/multiply/<num>", methods=["GET", "POST"])
 def multiply_int(num):
     """This function multiplies the number by 2."""
-    if request.method not in ('GET', 'POST'):
-        return {'error': "content type not supported."}, 405
+    if request.method not in ("GET", "POST"):
+        return {"error": "content type not supported."}, 405
     if num.isnumeric():
-        return {'value': float(num)*2}
-    return {'error': 'expected numbers, got string'}
+        return {"value": float(num) * 2}
+    return {"error": "expected numbers, got string"}
+
 
 def calc_salary(content):
     """This function calculates the salary of a person."""
-    salary, bonus, taxes = content.get("salary"), content.get("bonus"), content.get("taxes")
+    salary, bonus, taxes = (
+        content.get("salary"),
+        content.get("bonus"),
+        content.get("taxes"),
+    )
     if salary.isnumeric() and bonus.isnumeric() and taxes.isnumeric():
         salary, bonus, taxes = int(salary), int(bonus), int(taxes)
         return salary + bonus - taxes
     return False
 
+
 @app.route("/salary", methods=["POST", "GET"])
 def salary_api():
     """This API gets salary information for the person.
     :return: The salary of the person."""
-    if request.method not in ('GET', 'POST'):
-        return {'error': "content type not supported."}, 405
+    if request.method not in ("GET", "POST"):
+        return {"error": "content type not supported."}, 405
     content = request.args
     result = calc_salary(content)
     if result:
--- test_api.py
+++ test_api.py
@@ -1,27 +1,24 @@
 """This module is for testing flack_challenge.py API"""
 
+from io import BytesIO
+import json
 import requests
 import pycurl
-from io import BytesIO, StringIO
-import json
 
-def binary_to_dict(the_binary):
-    jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
-    d = json.loads(jsn)  
-    return d
 
 def test_multiply():
     """This is a test function for the API."""
     crl = pycurl.Curl()
     buffer = BytesIO()
-    #buffer = StringIO()
+    # buffer = StringIO()
     crl.setopt(crl.URL, "http://127.0.0.1:5000/multiply/56")
     crl.setopt(crl.WRITEDATA, buffer)
     crl.perform()
     crl.close()
     response = buffer.getvalue()
-    my_dictonary = dict(json.loads(response.decode('utf-8')))
+    my_dictonary = dict(json.loads(response.decode("utf-8")))
     print(my_dictonary)
+
 
 def test_api():
     """This is a test function for the API."""
@@ -50,4 +47,3 @@ def test_api():
 if __name__ == "__main__":
     test_api()
     test_multiply()
-
